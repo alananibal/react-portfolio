2579c8e07cc3146db833df21c7e9cb9268b068ac
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-08-08 21:32:12 -0700

    Updates

---
+++ precache-manifest.136f6dd71132c28567c849ac33c41b51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51db286929ef438c40741c06bf1aab55",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0187b0da348de20bbba0",
+    "url": "./static/css/2.c222a0de.chunk.css"
+  },
+  {
+    "revision": "ac342e2aaa4258cd092c",
+    "url": "./static/css/main.8a220557.chunk.css"
+  },
+  {
+    "revision": "0187b0da348de20bbba0",
+    "url": "./static/js/2.27753f26.chunk.js"
+  },
+  {
+    "revision": "9acc95f1ac96051bcd584c94a520c371",
+    "url": "./static/js/2.27753f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac342e2aaa4258cd092c",
+    "url": "./static/js/main.e11019c7.chunk.js"
+  },
+  {
+    "revision": "fe41a73be8ae3ecc4e63",
+    "url": "./static/js/runtime-main.0a5e682f.js"
+  }
+]);
